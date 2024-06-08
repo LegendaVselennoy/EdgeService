0d470a5680401f1cad97f35e6ea43ffd777847e6
LegendaVselennoy <rom.ahmedhanov@yandex.ru>
2024-06-08 19:18:07 +0300

    redis

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,12 +2,38 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+// Загружает полный контекст Web-приложения Spring и Web-окружение, прослушивающее случайный порт
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers  // Активирует автоматический запуск и очистку тестовых контейнеров
 class EdgeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static final int REDIS_PORT = 6379;
+
+	@Container
+	// Определяет контейнер Redis для тестирования
+	static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.0"))
+			.withExposedPorts(REDIS_PORT);
+
+
+	@DynamicPropertySource
+	// Перезаписывает конфигурацию Redis, чтобы она указывала на тестовый экземпляр Redis
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host",
+				() -> redis.getHost());
+		registry.add("spring.redis.port",
+				() -> redis.getMappedPort(REDIS_PORT));
 	}
 
+	// Пустой тест, используемый для проверки правильности загрузки контекста приложения и успешного подключения к Redis
+	@Test
+	void verifyThatSpringContextLoads(){
+
+	}
 }
